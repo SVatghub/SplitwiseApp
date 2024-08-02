0da91a254284679823aca5d4efc8d3f14cf1f659
sahil <sahil0000002@gmail.com>
2024-08-02 13:55:50 +0530

    Added Repo layer for all the entities.

--- src/main/java/com/sahil/SplitwiseApp/repo/DebtUsersRepo.java
+++ src/main/java/com/sahil/SplitwiseApp/repo/DebtUsersRepo.java
@@ -4,6 +4,12 @@ import com.sahil.SplitwiseApp.model.DebtUsers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface DebtUsersRepo extends JpaRepository<DebtUsers,Integer> {
+    List<DebtUsers> getDebtUsersByExpenseId(int expenseId);
+    List<DebtUsers> getDebtUsersByUserId(int userId);
+    Optional<DebtUsers> getDebtUserByExpenseIdAndUserId(int expenseId,int userId);
 }
--- src/main/java/com/sahil/SplitwiseApp/repo/ExpensesRepo.java
+++ src/main/java/com/sahil/SplitwiseApp/repo/ExpensesRepo.java
@@ -4,6 +4,10 @@ import com.sahil.SplitwiseApp.model.Expenses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ExpensesRepo extends JpaRepository<Expenses,Integer> {
+    List<Optional<Expenses>> findByUserId(int userId);
 }
