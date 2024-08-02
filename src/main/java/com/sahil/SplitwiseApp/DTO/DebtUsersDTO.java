409324cc1edfb16a07a4fc15444d1eb488be04b9
sahil <sahil0000002@gmail.com>
2024-08-02 13:56:49 +0530

    Added DTO for the DebtUsers.java

---
+++ src/main/java/com/sahil/SplitwiseApp/DTO/DebtUsersDTO.java
@@ -0,0 +1,71 @@
+package com.sahil.SplitwiseApp.DTO;
+import com.sahil.SplitwiseApp.model.Expenses;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+public class DebtUsersDTO {
+    private int id;
+    private int userId;
+    private int expenseId;
+    private BigDecimal debtAmount;
+    private boolean isSettled;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getExpenseId() {
+        return expenseId;
+    }
+
+    public void setExpenseId(int expenseId) {
+        this.expenseId = expenseId;
+    }
+
+    public BigDecimal getDebtAmount() {
+        return debtAmount;
+    }
+
+    public void setDebtAmount(BigDecimal debtAmount) {
+        this.debtAmount = debtAmount;
+    }
+
+    public boolean isSettled() {
+        return isSettled;
+    }
+
+    public void setSettled(boolean settled) {
+        isSettled = settled;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
