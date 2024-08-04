890972c111c6171de1b0a22b59b5f7d494f59603
sahil <sahil0000002@gmail.com>
2024-08-04 15:13:56 +0530

    Added lombok annotations in DTO.

--- src/main/java/com/sahil/SplitwiseApp/DTO/DebtUsersDTO.java
+++ src/main/java/com/sahil/SplitwiseApp/DTO/DebtUsersDTO.java
@@ -1,9 +1,13 @@
 package com.sahil.SplitwiseApp.DTO;
-import com.sahil.SplitwiseApp.model.Expenses;
 
+import com.sahil.SplitwiseApp.model.DebtUsers;
+import lombok.Getter;
+import lombok.Setter;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 
+@Getter
+@Setter
 public class DebtUsersDTO {
     private int id;
     private int userId;
@@ -12,60 +16,4 @@ public class DebtUsersDTO {
     private boolean isSettled;
     private Timestamp createdAt;
     private Timestamp updatedAt;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public int getExpenseId() {
-        return expenseId;
-    }
-
-    public void setExpenseId(int expenseId) {
-        this.expenseId = expenseId;
-    }
-
-    public BigDecimal getDebtAmount() {
-        return debtAmount;
-    }
-
-    public void setDebtAmount(BigDecimal debtAmount) {
-        this.debtAmount = debtAmount;
-    }
-
-    public boolean isSettled() {
-        return isSettled;
-    }
-
-    public void setSettled(boolean settled) {
-        isSettled = settled;
-    }
-
-    public Timestamp getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Timestamp createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Timestamp getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Timestamp updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
