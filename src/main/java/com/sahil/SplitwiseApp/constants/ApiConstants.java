30cb704090e9e5c069f128d419c3080e07f11895
sahil <sahil0000002@gmail.com>
2024-08-05 12:47:46 +0530

    Added Kebab case to userId.

--- src/main/java/com/sahil/SplitwiseApp/constants/ApiConstants.java
+++ src/main/java/com/sahil/SplitwiseApp/constants/ApiConstants.java
@@ -2,5 +2,5 @@ package com.sahil.SplitwiseApp.constants;
 
 public class ApiConstants {
     public static final String BASE_URL = "/Splitwise-App";
-    public static final String BASE_URL_EXPENSES = "/Splitwise-App/users/{userId}/expenses";
+    public static final String BASE_URL_EXPENSES = "/Splitwise-App/users/{user-Id}/expenses";
 }
