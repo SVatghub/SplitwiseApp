2b70eaa69a37aa00ec617484b7ebac796c1415c2
sahil <sahil0000002@gmail.com>
2024-08-07 18:15:01 +0530

    Added two methods to access a user with name and email and to get all users.

--- src/main/java/com/sahil/SplitwiseApp/service/UsersService.java
+++ src/main/java/com/sahil/SplitwiseApp/service/UsersService.java
@@ -4,6 +4,7 @@ import com.sahil.SplitwiseApp.model.Users;
 import com.sahil.SplitwiseApp.repo.UsersRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class UsersService {
@@ -17,5 +18,13 @@ public class UsersService {
 
     public Users getUserById(int userId){
         return repo.findById(userId).orElse(null);
+    }
+
+    public Integer getUserIdByNameAndEmail(String name, String email) {
+        return repo.getUserIdByNameAndEmail(name,email);
+    }
+
+    public List<Users> getAllUsers() {
+        return repo.findAll();
     }
 }
