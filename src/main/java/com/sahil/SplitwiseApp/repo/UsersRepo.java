87e11dfd4ec55865dea9d9774bed8867d1b7ba6b
sahil <sahil0000002@gmail.com>
2024-08-07 18:15:56 +0530

    Added the get User By Name and Email method in repo using a custom query.

--- src/main/java/com/sahil/SplitwiseApp/repo/UsersRepo.java
+++ src/main/java/com/sahil/SplitwiseApp/repo/UsersRepo.java
@@ -2,8 +2,12 @@ package com.sahil.SplitwiseApp.repo;
 
 import com.sahil.SplitwiseApp.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersRepo extends JpaRepository<Users,Integer> {
+
+    @Query("select u.id from Users u where u.name = ?1 and u.email = ?2")
+    Integer getUserIdByNameAndEmail(String name, String email);
 }
